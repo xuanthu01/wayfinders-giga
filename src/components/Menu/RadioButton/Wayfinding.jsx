e43d01196e27c71db3510efd04e2e9fd41482508
thupx <xuanthu404@gmail.com>
2019-09-25 11:03:04 +0700

    add AppContext

--- src/components/Menu/RadioButton/Wayfinding.jsx
+++ src/components/Menu/RadioButton/Wayfinding.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { If, removeShortestPathEl } from "../../../shared";
+import { If, removeShortestPathEl, showNodes, hideEdges } from "../../../shared";
 import { drawShortestPath } from "../../../helpers";
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { AppContext } from '../../../contexts/app.context';
 
 
 const PathStep = ({ step, index }) => {
-    // console.log("step:", step.join("=>").toString());
-
     return (
         <p id="node-pathline-list" style={{ whiteSpace: "nowrap", overflow: "auto", }}>
             {`Step ${index} : ${step.join("=>").toString()}`}
@@ -14,101 +13,101 @@ const PathStep = ({ step, index }) => {
     )
 }
 const VertextureComponent = (props) => {
-
+    const { vertex1, vertex2, setVertex, route, shortestPath, setShortestPath } = useContext(AppContext);
     const _drawShorestPath = () => {
-        console.log(oldVertex1, oldVertex2);
-        console.log(props.vertex1, props.vertex2);
-        if (oldVertex1 !== "" && oldVertex2 !== "") {
-            console.log("here");
-            removeShortestPathEl(oldVertex1, oldVertex2);
+        try {
+            console.log("vertex1:", vertex1);
+            console.log("vertex2:", vertex2);
+
+
+            // console.log(oldVertex1, oldVertex2);
+            // console.log(props.vertex1, props.vertex2);
+            if (vertex1 !== "" && vertex2 !== "") {
+                console.log("here");
+                removeShortestPathEl(oldVertex1, oldVertex2);
+            }
+            else if (vertex1 !== "" && vertex2 !== "") {
+                removeShortestPathEl(vertex1, vertex2);
+                setVertex({ vertex1: "", vertex2: "" });
+            }
+            console.log(vertex1, vertex2, "vertẽx");
+            if (vertex1.length === 0 && vertex2.length === 0) {
+                console.log("can not get vertexid");
+            }
+            setShortestPath(drawShortestPath(vertex1, vertex2, route));
+            if (shortestPath === undefined || null)
+                return;
+            var result = Object.keys(shortestPath).map(function (key) {
+                return [shortestPath[key]];
+            });
+            let pElement = document.getElementById("node-pathline-list")
+            pElement.innerText = result.join("=>");
+            // setOldVertex1(vertex1);
+            // setOldVertex2(vertex2);
+            setVertex({ vertex1: vertex1, vertex2: vertex2 })
+            // props.changeVertex(vertex1, vertex2);
+        } catch (error) {
+            console.log("error in _drawShorestPath:", error);
         }
 
-        else if (props.vertex1 !== "" && props.vertex2 !== "") {
-
-            removeShortestPathEl(props.vertex1, props.vertex2);
-            props.resetVertex();
-        }
-        // console.log("VertextureComponent");
-        // removeShortestPathEl(props.vertex1,props.vertex2);  
-        console.log(vertex1, vertex2, "vertẽx");
-        if (vertex1.length === 0 && vertex2.length === 0) {
-            console.log("can not get vertexid");
-        }
-        let pathArr = drawShortestPath(vertex1, vertex2, props.route);
-        if (pathArr === undefined || null)
-            return;
-        var result = Object.keys(pathArr).map(function (key) {
-            return [pathArr[key]];
-        });
-        let pElement = document.getElementById("node-pathline-list")
-        pElement.innerText = result.toString();
-        // setOldVertex1(vertex1);
-        // setOldVertex2(vertex2);
-        props.changeVertex(vertex1, vertex2);
     }
-    const [vertex1, setInputVertex1] = useState('');
-    const [vertex2, setInputVertex2] = useState('');
     const [oldVertex1] = useState('');
     const [oldVertex2] = useState('');
 
-    if (props.pathArr === undefined || null)
+    if (shortestPath === undefined || null)
         return;
-    var result = Object.keys(props.pathArr).map(function (key) {
-        return [props.pathArr[key]];
+    var result = Object.keys(shortestPath).map(function (key) {
+        return [shortestPath[key]];
     });
     // console.log("result:", result);
 
     return (
         <div>
-            <input type="text" id="first-vertex" onChange={e => {
-
-                setInputVertex1(e.target.value);
-
+            <input type="text" id="first-vertex" onBlur={e => {
+                // setInputVertex1(e.target.value);
+                setVertex({ vertex1: e.target.value });
             }} />
             <span>  </span>
-            <input type="text" id="second-vertex" onChange={e => {
+            <input type="text" id="second-vertex" onBlur={e => {
+                // setInputVertex2(e.target.value);
+                setVertex({ vertex2: e.target.value });
 
-                setInputVertex2(e.target.value);
-                console.log(vertex2, e.target.value);
-
+                // console.log(vertex2, e.target.value);
             }} />
             <span>  </span>
             <button onClick={_drawShorestPath}>Find</button> <br />
-
             {
-                // props !== {} ? <p id="node-pathline-list" style={{ whiteSpace: "nowrap", overflow: "auto", }}>{result.join("=>")}</p> : null
-                props !== {} ? result.map((step, index) => {
+                result.map((step, index) => {
                     return <><p>List of path</p>
                         <PathStep key={index} step={step} index={index + 1} /></>
-
-                }) : null
+                })
             }
         </div>)
 }
 class WayFindRadioButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.OnWayFinding = this.OnWayFinding.bind(this)
-    }
-    OnWayFinding = () => {
-        console.log("OnWayFinding");
-        this.props.OnWayFinding();
-
-    };
+    static contextType = AppContext;
     shouldComponentUpdate(nextProps, nextState) {
-        if (this.props.pathArr !== nextProps.pathArr)
+        if (this.props.shortestPath !== nextProps.shortestPath)
             return true;
         return this.props.feature !== nextProps.feature
     }
     render() {
-        const condition = this.props.feature === 'find';
-        const { pathArr, route, changeVertex, vertex2, vertex1, resetVertex } = this.props;
+        const condition = this.context.feature === 'find';
+        // const { shortestPath, route, setVertex, vertex2, vertex1 } = this.context;
         return (
-            <>
-                <input type="radio" id="way-Finding" onChange={this.OnWayFinding} name="chooseFeature" />Way Finding <br />
-                <If condition={condition} component={VertextureComponent} props={{ pathArr: pathArr, route: route, changeVertex: changeVertex, vertex1: vertex1, vertex2: vertex2, resetVertex: resetVertex }} />
+            <AppContext.Consumer>
+                {({ setFeature }) => (
+                    <>
+                        <input type="radio" id="way-Finding" onChange={() => {
+                            setFeature("find");
+                            showNodes(true);
+                            hideEdges();
+                        }} name="chooseFeature" />Way Finding <br />
+                        <If condition={condition} component={VertextureComponent} />
+                    </>
+                )}
+            </AppContext.Consumer>
 
-            </>
         )
     }
 }
